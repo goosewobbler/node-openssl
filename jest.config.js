73932df9921dd137d26dae5b63caccf20a288772
goosewobbler <goosewobbler@pm.me>
2022-03-29 19:06:00 +0100

    add more jest conf

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.(ts|tsx)'],
+  collectCoverageFrom: ['src/**/*'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  clearMocks: true,
 };
